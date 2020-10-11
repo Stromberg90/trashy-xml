79b895d7782e07376b2cac8c10fbf7456d998446
Stromberg90 <andreas.stromberg90@gmail.com>
2020-10-11 11:33:04 +0200

    Lexer refactoring

--- src/lexer.rs
+++ src/lexer.rs
@@ -13,29 +13,30 @@ pub fn next(xml_parser: &mut XmlParser) -> Option<u8> {
     } else {
         xml_parser.started_parsing = true;
     }
-    if let Some(character) = xml_parser.buffer.get(xml_parser.index as usize).copied() {
-        match character {
-            b'\r' => {
-                if let Some(v) = peek(xml_parser) {
-                    if v == b'\n' {
-                        xml_parser.index += 1;
-                        xml_parser.position.line += 1;
-                        xml_parser.position.column = 1;
-                    }
+    if xml_parser.index as usize >= xml_parser.buffer.len() {
+        return None;
+    }
+    let character = xml_parser.buffer[xml_parser.index as usize];
+    match character {
+        b'\r' => {
+            if let Some(v) = peek(xml_parser) {
+                if v == b'\n' {
+                    xml_parser.index += 1;
+                    xml_parser.position.line += 1;
+                    xml_parser.position.column = 1;
                 }
             }
-            b'\t' => {
-                xml_parser.position.column += xml_parser.settings.tab_width as u32;
-            }
-            b'\n' => {
-                xml_parser.position.line += 1;
-                xml_parser.position.column = 1;
-            }
-            _ => {
-                xml_parser.position.column += 1;
-            }
         }
-        return Some(character);
+        b'\t' => {
+            xml_parser.position.column += xml_parser.settings.tab_width as u32;
+        }
+        b'\n' => {
+            xml_parser.position.line += 1;
+            xml_parser.position.column = 1;
+        }
+        _ => {
+            xml_parser.position.column += 1;
+        }
     }
-    None
+    return Some(character);
 }
